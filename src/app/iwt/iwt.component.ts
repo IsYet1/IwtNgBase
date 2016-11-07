f12975a414c01b2a501982c7a1c348837de17e98
IsYet1 <Don@IsYet.com>
2016-11-06 18:44:12 -0600

    Add iwt module to app.module.

--- src/app/iwt/iwt.component.ts
+++ src/app/iwt/iwt.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-iwt',
+  selector: 'iwt',
   templateUrl: './iwt.component.html',
   styleUrls: ['./iwt.component.css']
 })
