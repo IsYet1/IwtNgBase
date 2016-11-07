63fb89b2322e9f3fdb788edca150f9c456fdda41
IsYet1 <Don@IsYet.com>
2016-11-06 18:37:08 -0600

    Add module. Change prefix.

---
+++ src/app/iwt/iwt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iwt',
+  templateUrl: './iwt.component.html',
+  styleUrls: ['./iwt.component.css']
+})
+export class IwtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/iwt/iwt.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IwtComponent } from './iwt.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [IwtComponent]
+})
+export class IwtModule { }
